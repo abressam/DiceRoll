94409f59267fb6c4810a44a3d288ac56083ce435
Amanda Bressam <amandabressam@gmail.com>
2022-01-15 09:34:19 -0300

    Created the function rollDice() and defined it inside the rollButton.setOnClickListener

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -10,11 +10,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
+        val rollButton: Button = findViewById(R.id.button) // find the Button by calling findViewById
+        rollButton.setOnClickListener { // setOnClickListener is used to made an event when the button is clicked
+            rollDice() // call the function rollDice when the button is clicked
         }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6) // created a 6 sides dice
+        val diceRoll = dice.roll() // roll the dice calling the roll() method and save the result in the diceRoll variable
+        val resultTextView: TextView = findViewById(R.id.textView) // find TextView by calling findViewById
+        resultTextView.text = diceRoll.toString() // diceRoll is a number and TextView uses text, so need to convert diceRoll into a string
     }
 
     class Dice(private val numSides: Int) { // numSides is only accessible within the Dice class
