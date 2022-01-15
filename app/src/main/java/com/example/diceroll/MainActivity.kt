460885f627f17f3b0d6232d9806cb9edfd9db5c4
Amanda Bressam <amandabressam@gmail.com>
2022-01-15 09:46:53 -0300

    Created comments, clean up code and reformed the code by using (Edit -> Select All) (Code -> Reformat Code)

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -5,22 +5,33 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.button) // find the Button by calling findViewById
-        rollButton.setOnClickListener { // setOnClickListener is used to made an event when the button is clicked
-            rollDice() // call the function rollDice when the button is clicked
-        }
+        val rollButton: Button =
+            findViewById(R.id.button) // find the Button by calling findViewById
+        rollButton.setOnClickListener { rollDice() }
+        // setOnClickListener is used to made an event when the button is clicked
+        // inside setOnClick Listener is called the function rollDice() when the button is clicked
     }
 
+    /**
+     * Roll the dice and update the screen with the result.
+     */
     private fun rollDice() {
         val dice = Dice(6) // created a 6 sides dice
-        val diceRoll = dice.roll() // roll the dice calling the roll() method and save the result in the diceRoll variable
-        val resultTextView: TextView = findViewById(R.id.textView) // find TextView by calling findViewById
-        resultTextView.text = diceRoll.toString() // diceRoll is a number and TextView uses text, so need to convert diceRoll into a string
+        val diceRoll = dice.roll()
+        // roll the dice calling the roll() method and save the result in the diceRoll variable
+        val resultTextView: TextView = findViewById(R.id.textView)
+        // find TextView by calling findViewById
+        resultTextView.text = diceRoll.toString()
+        // diceRoll is a number and TextView uses text, so need to convert diceRoll into a string
     }
 
     class Dice(private val numSides: Int) { // numSides is only accessible within the Dice class
